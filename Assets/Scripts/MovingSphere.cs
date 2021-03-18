2d6a7fbebd0a76141719fe16018d309614f1bec5
liuyongfeng <531817981@qq.com>
2021-03-18 13:53:01 +0800

    -> physics

---
+++ Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    readonly float m_maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    readonly float m_maxAcceleration = 10f;
+
+    [SerializeField]
+    Rect m_allowedArea = new(-5f, -5f, 10f, 10f);
+
+    [SerializeField, Range(0f, 1f)]
+    readonly float bounciness = 0.5f;
+
+    Vector3 m_velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * m_maxSpeed;
+        float maxSpeedChange = m_maxAcceleration * Time.deltaTime; // 直接用max了
+        m_velocity.x = Mathf.MoveTowards(m_velocity.x, desiredVelocity.x, maxSpeedChange);
+        m_velocity.z = Mathf.MoveTowards(m_velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        Vector3 newPosition = transform.position + m_velocity * Time.deltaTime;
+        // if (!m_allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+        // {
+
+        //     newPosition.x = Mathf.Clamp(newPosition.x, m_allowedArea.xMin, m_allowedArea.xMax);
+        //     newPosition.z = Mathf.Clamp(newPosition.z, m_allowedArea.yMin, m_allowedArea.yMax);
+        //     //todo 加入弹性
+        // }
+
+        if (newPosition.x < m_allowedArea.xMin)
+        {
+            newPosition.x = m_allowedArea.xMin;
+            m_velocity.x = -m_velocity.x * bounciness;
+        }
+        else if (newPosition.x > m_allowedArea.xMax)
+        {
+            newPosition.x = m_allowedArea.xMax;
+            m_velocity.x = -m_velocity.x * bounciness;
+        }
+        else if (newPosition.z < m_allowedArea.yMin)
+        {
+            newPosition.z = m_allowedArea.yMax;
+            // m_velocity.z = -m_velocity.z;
+        }
+        else if (newPosition.z > m_allowedArea.yMax)
+        {
+            newPosition.z = m_allowedArea.yMin;
+            // m_velocity.z = -m_velocity.z;
+        }
+
+        transform.localPosition = newPosition;
+
+
+    }
+}
---
+++ Assets/Scripts/MovingSphere2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere2 : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float m_maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float m_maxAcceleration = 10f;
+
+    Vector3 m_velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * m_maxSpeed;
+        float maxSpeedChange = m_maxAcceleration * Time.deltaTime; // 直接用max了
+        m_velocity.x = Mathf.MoveTowards(m_velocity.x, desiredVelocity.x, maxSpeedChange);
+        m_velocity.z = Mathf.MoveTowards(m_velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        Vector3 newPosition = transform.position + m_velocity * Time.deltaTime;
+
+        transform.localPosition = newPosition;
+
+
+    }
+}
